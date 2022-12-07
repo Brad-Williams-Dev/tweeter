1f69f1ce8d7c088c8679041a5893d940dc3d7c41
Brad Williams <bradley_williams2003@hotmail.com>
2022-12-07 09:12:07 -0400

    added added functions to dynamically generate tweets onto index.html page. All working as it should

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+
+// Test / driver code (temporary). Eventually will get this from the server.
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+
+$(document).ready(function() {
+
+  const createTweetElement = tweet => {
+    let $tweet = `
+    <article class="tweet">
+      <header>
+        <div class="tweet-avatar"><img src="${tweet.user.avatars}"></img>${tweet.user.name}</div>
+        <div class="tweet-handle">${tweet.user.handle}</div>
+      </header>
+      <p>${tweet.content.text}</p>
+      <footer>
+        <div class="tweet-date">${tweet.created_at}</div>
+        <div class="tweet-icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer >
+    </article>;
+    `;
+    return $tweet;
+  };
+
+  const renderTweets = tweets => {
+
+    for (const tweet of tweets) {
+      let newTweet = createTweetElement(tweet);
+      $('.tweets-container').append(newTweet);
+    }
+
+  };
+
+  renderTweets(data);
+});
