b77ce9c157bd46337f5a523592a3b4dfb7e4df80
Brad Williams <bradley_williams2003@hotmail.com>
2022-12-08 11:06:09 -0400

    added responsive css to make page responsive to desktop and tablet screens

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,54 +3,15 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-
 $(document).ready(function() {
 
-  $('form').submit(function(event) {
-    event.preventDefault();
-    let newData = $(this).serialize();
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-    $.ajax({
-      type: 'POST',
-      url: '/tweets/',
-      data: newData
-    });
-
-    console.log(newData);
-
-  });
-
+  // Creates new HTML for new tweets
   const createTweetElement = tweet => {
     let $tweet = `
     <article class="tweet">
@@ -58,13 +19,17 @@ $(document).ready(function() {
         <div class="tweet-avatar"><img src="${tweet.user.avatars}"></img>${tweet.user.name}</div>
         <div class="tweet-handle">${tweet.user.handle}</div>
       </header>
-      <p>${tweet.content.text}</p>
+      <div id="tweet-inner">
+      <p>${escape(tweet.content.text)}</p>
+      </div>
       <footer>
-        <div class="tweet-date">${tweet.created_at}</div>
+        <div class="tweet-date">${timeago.format(tweet.created_at)}</div>
         <div class="tweet-icons">
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
+          <table>
+            <td class="icon"><i class="fa-solid fa-flag"></i></td>
+            <td class="icon"> <i class="fa-solid fa-retweet"></i></td>
+            <td class="icon"><i class="fa-solid fa-heart"></i></td>
+          </table>
         </div>
       </footer >
     </article>;
@@ -72,14 +37,55 @@ $(document).ready(function() {
     return $tweet;
   };
 
+  // Loops through list of tweets and runs them through createTweetElement function, then prepends them to index.html page
   const renderTweets = tweets => {
 
+
     for (const tweet of tweets) {
-      let newTweet = createTweetElement(tweet);
-      $('.tweets-container').append(newTweet);
+      let $newTweet = createTweetElement(tweet);
+      $('.tweets-container').prepend($newTweet);
     }
 
   };
 
-  renderTweets(data);
+  // AJAX GET request from tweet server and runs each tweet through renderTweets function.
+  const loadTweets = function() {
+
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function(allTweets) {
+        renderTweets(allTweets);
+      });
+
+  };
+
+
+  // Takes data from the forms submission, serializes it, then POSTS it to the tweet server
+  $('form').submit(function(event) {
+    event.preventDefault();
+    let input = this.children[1].value;
+
+    // Error slides for if message fiels is empty or over 140 char
+    if (!input) {
+      $('#error').slideDown().text('⚠️ Message field was empty, please write a message to post! ⚠️').css({ display: 'flex' });
+    } else if (input.length > 140) {
+      $('#error').slideDown().text('⚠️ Message to long! Please be sure your message is less than 140 characters! ⚠️').css({ display: 'flex' });;
+    } else {
+      $('#error').hide();
+      const newData = $(this).serialize();
+
+      // AJAX post request that fetches tweets from database, then reloads loadTweets function, then calls javascripts reset method to reset the form.
+      $.ajax({
+        type: 'POST',
+        url: '/tweets',
+        data: newData
+      })
+        .then(() => loadTweets())
+        .catch((error) => { console.log("Error: ", error); });
+      this.reset();
+
+    }
+  });
+
+  // Calls the loadTweets function 
+  loadTweets();
 });
